ff761dcceee96f6eadbe14249060f5ca71d3a512
I'm Sanji <filesharingbot78@gmail.com>
2024-06-11 08:58:35 +0530

    Add files via upload
    Signed-off-by: I'm Sanji  <filesharingbot78@gmail.com>

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -55,7 +55,7 @@ async def init():
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("AnonXMusic").info("Stopping Sanjii X Music Bot...")
+    LOGGER("AnonXMusic").info("Stopping Music Bot...")
 
 
 if __name__ == "__main__":
