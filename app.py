1b034ca3db0259fcb535151eceb4c5a8d4782e6d
sagato-bots <jvebych376@adefesio.homes>
2024-06-12 19:25:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "Sagato-bots"
+    return '@ahmedbots'
 
 
 if __name__ == "__main__":
     app.run()
+
